37cdb307f3c36af799aa9fcf7d5e71f961191f34
Karthik Nanjundaswamy Guru Prasad <karthiknanjundswamyguruprasad@gmail.com>
2015-06-14 17:31:16 -0400

    fixed bugs of committing

--- app/src/main/java/app/com/example/karthik/recipemania/Fragment_About.java
+++ app/src/main/java/app/com/example/karthik/recipemania/Fragment_About.java
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 package app.com.example.karthik.recipemania;
 
 import android.app.Fragment;
@@ -26,8 +24,8 @@ public class Fragment_About extends Fragment {
         // Inflate the layout for this fragment
 
         v = inflater.inflate(R.layout.help, container, false);
-        ((ActionBarActivity)getActivity()).getSupportActionBar().setTitle("About Us");
-        final ImageButton imageButton=(ImageButton)v.findViewById(R.id.imageView);
+        ((ActionBarActivity) getActivity()).getSupportActionBar().setTitle("About Us");
+        final ImageButton imageButton = (ImageButton) v.findViewById(R.id.imageView);
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -39,50 +37,4 @@ public class Fragment_About extends Fragment {
         return v;
 
     }
-=======
-=======
->>>>>>> origin/master
-package app.com.example.karthik.recipemania;
-
-import android.app.Fragment;
-import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageButton;
-
-import com.daimajia.androidanimations.library.Techniques;
-import com.daimajia.androidanimations.library.YoYo;
-
-/**
- * Created by Karthik on 4/24/2015.
- */
-public class Fragment_About extends Fragment {
-
-    View v;
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-
-        v = inflater.inflate(R.layout.help, container, false);
-        ((ActionBarActivity)getActivity()).getSupportActionBar().setTitle("About Us");
-        final ImageButton imageButton=(ImageButton)v.findViewById(R.id.imageView);
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                YoYo.with(Techniques.Flash)
-                        .duration(1700)
-                        .playOn(imageButton);
-            }
-        });
-        return v;
-
-    }
-<<<<<<< HEAD
->>>>>>> origin/master
-=======
->>>>>>> origin/master
 }
--- app/src/main/java/app/com/example/karthik/recipemania/HomeFragment.java
+++ app/src/main/java/app/com/example/karthik/recipemania/HomeFragment.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package app.com.example.karthik.recipemania;
 
 import android.os.Bundle;
@@ -50,49 +49,3 @@ public class HomeFragment extends android.app.Fragment {
         return rootView;
     }
 }
-=======
-package app.com.example.karthik.recipemania;
-
-import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageButton;
-
-import com.daimajia.androidanimations.library.Techniques;
-import com.daimajia.androidanimations.library.YoYo;
-
-/**
- * Created by Karthik on 4/20/2015.
- */
-public class HomeFragment extends android.app.Fragment {
-
-    ImageButton imageButton;
-    public HomeFragment(){}
-
-
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-        View rootView = inflater.inflate(R.layout.homefragment, container, false);
-
-        ((ActionBarActivity)getActivity()).getSupportActionBar().setTitle("RecipeMania");
-
-        imageButton=(ImageButton)rootView.findViewById(R.id.homeimage);
-
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                    YoYo.with(Techniques.StandUp)
-                            .duration(700)
-                            .playOn(imageButton);
-            }
-        });
-
-
-        return rootView;
-    }
-}
->>>>>>> origin/master
