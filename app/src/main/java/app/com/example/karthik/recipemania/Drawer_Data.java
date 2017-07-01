2e846ffdc8755e081bc9d4d6d369a170cf0d5e47
Karthik Prasad <karthikprasad328@gmail.com>
2017-07-01 17:06:53 -0400

    Removing Adview and code refactoring

--- app/src/main/java/app/com/example/karthik/recipemania/Drawer_Data.java
+++ app/src/main/java/app/com/example/karthik/recipemania/Drawer_Data.java
@@ -9,17 +9,17 @@ import java.util.Map;
  * Created by Karthik on 4/19/2015.
  */
 public class Drawer_Data {
-    List<Map<String,?>> drawerList;
+    List<Map<String, ?>> drawerList;
 
     public List<Map<String, ?>> getDrawerList() {
         return drawerList;
     }
 
-    public int getSize(){
+    public int getSize() {
         return drawerList.size();
     }
 
-    public HashMap getItem(int i){
+    public HashMap getItem(int i) {
         return (HashMap) drawerList.get(i);
     }
 
@@ -27,25 +27,28 @@ public class Drawer_Data {
         HashMap item;
         drawerList = new ArrayList<Map<String, ?>>();
         item = new HashMap();
-        item.put("icon", R.drawable.homeicon); item.put("title", "Home");
+        item.put("icon", R.drawable.homeicon);
+        item.put("title", "Home");
         drawerList.add(item);
         item = new HashMap();
-        item.put("icon", R.drawable.popularicon); item.put("title", "Popular Recipes");
+        item.put("icon", R.drawable.popularicon);
+        item.put("title", "Popular Recipes");
         drawerList.add(item);
         item = new HashMap();
-        item.put("icon", R.drawable.fridgesearch); item.put("title", "Search By Ingredients");
+        item.put("icon", R.drawable.fridgesearch);
+        item.put("title", "Search By Ingredients");
         drawerList.add(item);
         item = new HashMap();
-        item.put("icon", R.drawable.favouritesicon); item.put("title", "My Favourites");
-        drawerList.add(item);
-//        item = new HashMap();
-//        item.put("icon", R.drawable.calendar); item.put("title", "Planner");
-//        drawerList.add(item);
-        item = new HashMap();
-        item.put("icon", R.drawable.groceryicon); item.put("title", "Grocery List");
+        item.put("icon", R.drawable.favouritesicon);
+        item.put("title", "My Favourites");
         drawerList.add(item);
         item = new HashMap();
-        item.put("icon", R.drawable.help); item.put("title", "About");
+        item.put("icon", R.drawable.groceryicon);
+        item.put("title", "Grocery List");
+        drawerList.add(item);
+        item = new HashMap();
+        item.put("icon", R.drawable.help);
+        item.put("title", "About");
         drawerList.add(item);
     }
 }
